0cc535def987efc2840cf84a54aa1f341c24fab0
Mickaël <mickael@securem.eu>
2015-02-08 16:01:50 +0100

    Add argparse to the demologfeeder

--- demologfeeder.py
+++ demologfeeder.py
@@ -8,12 +8,17 @@ import random
 from datetime import datetime
 import config
 from display import Display
+import argparse
 
 
 if __name__ == '__main__':
-    # TODO: argument handling
-    logfile_path = '/tmp/demo.log'
-    delay = 2  # (seconds)
+    parser = argparse.ArgumentParser(description="Generate random hits in a HTTP access log")
+    parser.add_argument("logfile", help="Log file to write to")
+    parser.add_argument("-d", "--delay", default=1,
+                        help="Time to wait between to hits")
+    args = parser.parse_args()
+    logfile_path = args.logfile
+    delay = float(args.delay)  # (seconds)
     buffer_size = 1  # Line buffered
 
     http_response_codes = [200, 201, 204, 301, 400, 500]
@@ -23,7 +28,7 @@ if __name__ == '__main__':
             random_params = (random.randint(1, 5),
                              random.choice(http_response_codes))
 
-            log_line = '127.0.0.1 - - [23/Jul/2014:20:09:28 +0200] "GET /section%d/ HTTP/1.1" %d 1004 "-" "curl/7.32.0" "Apache/2.4.6 (Linux/SUSE) OpenSSL/1.0.1h"\n' \
+            log_line = '127.0.0.1 - - [23/Jul/2014:20:09:28 +0200] "GET /section%d/ HTTP/1.1" %d 1004 "-" "curl/7.32.0"\n' \
                 % random_params
 
             print("Simulated request on /section%d/ with response code %d" % random_params)
